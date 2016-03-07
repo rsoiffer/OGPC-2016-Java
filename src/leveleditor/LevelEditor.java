e18567f1379e8e8ded9362a3a456339e6324cbe3
Weirdbob95 <rory.soiffer@comcast.net>
2016-03-07 00:26:16 -0800

    Improved the level editor

--- src/leveleditor/LevelEditor.java
+++ src/leveleditor/LevelEditor.java
@@ -7,28 +7,15 @@ import graphics.Window2D;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.function.Function;
 import java.util.function.Supplier;
 import org.lwjgl.input.Keyboard;
 import ui.UIElement;
 import static ui.UIElement.space;
 import ui.UIList;
 import static ui.UIList.list;
-import ui.UISelector;
 import ui.UIShowOne;
-import ui.UIText;
-import static ui.UIText.text;
 import ui.UIValue;
 import util.Color4;
-import static util.Color4.BLACK;
-import static util.Color4.BLUE;
-import static util.Color4.GREEN;
-import static util.Color4.ORANGE;
-import static util.Color4.PURPLE;
-import static util.Color4.RED;
-import static util.Color4.WHITE;
-import static util.Color4.YELLOW;
 import util.Util;
 import util.Vec2;
 
@@ -36,7 +23,6 @@ public class LevelEditor {
 
     private static Supplier<Boolean> notOverUI;
     private static int uiMode;
-    private static Color4 serverColor = Color4.random();
 
     public static void main(String[] args) {
         //Game
@@ -44,8 +30,6 @@ public class LevelEditor {
 
         Tile.init();
         UI();
-
-        Input.whenKey(Keyboard.KEY_C, true).onEvent(() -> serverColor = Color4.random());
 
         Input.whenKey(Keyboard.KEY_O, true).onEvent(() -> {
             try {
@@ -95,7 +79,6 @@ public class LevelEditor {
         });
 
         Core.run();
-
     }
 
     private static void UI() {
@@ -115,7 +98,7 @@ public class LevelEditor {
 
         //Create ui
         UIElement tileUI = tileUI();
-        UIElement creatureUI = creatureUI();
+        UIElement creatureUI = raiseLowerUI();
         screen.add(tileUI, creatureUI);
         screen.color = () -> Color4.gray(.95);
         screen.border = true;
@@ -137,213 +120,35 @@ public class LevelEditor {
         });
     }
 
-    private static UIElement creatureUI() {
-        UIText name = text("Select an object");
-
-        Signal<Drawable> selected = new Signal(null);
-        UIElement selectedView = new UIElement(new Vec2(50)) {
-            @Override
-            public void draw() {
-                super.draw();
-                if (selected.get() != null) {
-                    selected.get().draw(pos.add(size.multiply(.5)));
-                }
-            }
-        };
-
-        UIShowOne health = new UIShowOne();
-        UIShowOne color = new UIShowOne();
-        selected.forEach(d -> {
-            if (d instanceof Creature) {
-                name.text = () -> "Creature";
-            } else if (d != null) {
-                name.text = () -> "Block";
-            } else {
-                name.text = () -> "Select an object";
-            }
-
-            health.showing = space(0);
-            health.parts.clear();
-
-            if (d != null) {
-                UIValue red = new UIValue("Red", x -> x < 32, x -> x > 0);
-                red.value.set(32);
-                UIValue green = new UIValue("Green", x -> x < 32, x -> x > 0);
-                green.value.set(32);
-                UIValue blue = new UIValue("Blue", x -> x < 32, x -> x > 0);
-                blue.value.set(32);
-                UIList colors = list(false, red, green, blue);
-                colors.gravity = 1;
-                Supplier<Color4> getColor = () -> new Color4(red.value.get() / 32., green.value.get() / 32., blue.value.get() / 32.);
-                Function<Supplier<Color4>, UIElement> colorButton = s -> {
-                    UIElement button = space(30);
-                    button.color = s;
-                    button.border = true;
-                    button.onClick.onEvent(() -> {
-                        Color4 c = s.get();
-                        red.value.set((int) Math.round(c.r * 32));
-                        green.value.set((int) Math.round(c.g * 32));
-                        blue.value.set((int) Math.round(c.b * 32));
-                        //showColor.color = () -> c;
-                    });
-                    UIShowOne r = new UIShowOne();
-                    r.add(button);
-                    r.showing = button;
-                    return r;
-                };
-                UIList chooseColor1 = list(true,
-                        colorButton.apply(() -> RED),
-                        colorButton.apply(() -> ORANGE),
-                        colorButton.apply(() -> YELLOW),
-                        colorButton.apply(() -> GREEN),
-                        colorButton.apply(() -> BLUE),
-                        colorButton.apply(() -> PURPLE));
-                UIList chooseColor2 = list(true,
-                        colorButton.apply(() -> WHITE),
-                        colorButton.apply(() -> Color4.gray(.75)),
-                        colorButton.apply(() -> Color4.gray(.5)),
-                        colorButton.apply(() -> Color4.gray(.25)),
-                        colorButton.apply(() -> BLACK),
-                        colorButton.apply(Core.interval(.5).map(Color4::random)));
-                chooseColor1.setAllPadding(new Vec2(10));
-                chooseColor2.setAllPadding(new Vec2(10));
-
-                red.value.combine(green.value, blue.value).onEvent(() -> {
-                    d.color = getColor.get();
-                });
-                UIList colorList = list(false, chooseColor1, chooseColor2, colors);
-                color.parts.add(colorList);
-                color.showing = colorList;
-                colorList.gravity = .5;
-            } else {
-                color.parts.clear();
-                color.showing = space(0);
-            }
-        });
-        selected.ofType(Creature.class).forEach(c -> {
-            UIValue healthValue = new UIValue("Health", x -> x < ((Creature) selected.get()).maxHealth, x -> x > 0);
-            healthValue.value.set(c.health);
-            healthValue.value.forEach(x -> {
-                c.health = x;
-            });
-            health.add(healthValue);
-            health.showing = healthValue;
-        });
-
-        UIList ui = list(false, name, selectedView, health, color);
-        ui.setAllPadding(new Vec2(10));
-        ui.gravity = .5;
-
-        Input.whenMouse(0, true).filter(() -> uiMode == 2).filter(notOverUI).onEvent(() -> {
-            Tile.tileAt(Input.getMouse()).ifPresent(t -> selected.set(t.drawable));
-        });
-
-        return ui;
+    private static UIElement raiseLowerUI() {
+        return null;
     }
 
     private static UIElement tileUI() {
-        UIValue red = new UIValue("Red", x -> x < 32, x -> x > 0);
-        red.value.set(32);
-        UIValue green = new UIValue("Green", x -> x < 32, x -> x > 0);
-        green.value.set(32);
-        UIValue blue = new UIValue("Blue", x -> x < 32, x -> x > 0);
-        blue.value.set(32);
-        UIList colors = list(false, red, green, blue);
-        colors.gravity = 1;
-        Supplier<Color4> getColor = () -> new Color4(red.value.get() / 32., green.value.get() / 32., blue.value.get() / 32.);
+        UIValue height = new UIValue("Height", x -> x < 32, x -> x > -32);
+        //height.value.set(32);
+        Supplier<Color4> getColor = height.value.map(v -> Color4.gray(v / 64. + .5));
 
-        UISelector typeSelector = new UISelector("Tile", Arrays.asList("Tile", "Block", "Creature", "Clear"), () -> true);
-        typeSelector.padding = new Vec2(20);
         UIElement showColor = new UIElement(new Vec2(200, 50));
         showColor.color = getColor;
         showColor.border = true;
 
-        Function<Supplier<Color4>, UIElement> colorButton = s -> {
-            UIElement button = space(30);
-            button.color = s;
-            button.border = true;
-            button.onClick.onEvent(() -> {
-                Color4 c = s.get();
-                red.value.set((int) Math.round(c.r * 32));
-                green.value.set((int) Math.round(c.g * 32));
-                blue.value.set((int) Math.round(c.b * 32));
-                //showColor.color = () -> c;
-            });
-            UIShowOne r = new UIShowOne();
-            r.add(button);
-            r.showing = button;
-            return r;
-        };
-        UIList chooseColor1 = list(true,
-                colorButton.apply(() -> RED),
-                colorButton.apply(() -> ORANGE),
-                colorButton.apply(() -> YELLOW),
-                colorButton.apply(() -> GREEN),
-                colorButton.apply(() -> BLUE),
-                colorButton.apply(() -> PURPLE));
-        UIList chooseColor2 = list(true,
-                colorButton.apply(() -> WHITE),
-                colorButton.apply(() -> Color4.gray(.75)),
-                colorButton.apply(() -> Color4.gray(.5)),
-                colorButton.apply(() -> Color4.gray(.25)),
-                colorButton.apply(() -> BLACK),
-                colorButton.apply(Core.interval(.5).map(Color4::random)));
-        chooseColor1.setAllPadding(new Vec2(10));
-        chooseColor2.setAllPadding(new Vec2(10));
-
-        UIValue brushSize = new UIValue("Brush Size", x -> x < 5, x -> x > 1);
+        UIValue brushSize = new UIValue("Brush Size", x -> x < 10, x -> x > 1);
         brushSize.value.set(1);
 
-        UIList ui = list(false, typeSelector, brushSize, space(15), showColor, chooseColor1, chooseColor2, colors);
+        UIList ui = list(false, space(25), showColor, space(10), height, brushSize);
         ui.gravity = .5;
+        ui.setAllPadding(new Vec2(10));
 
         //Key Input
         Input.whenMouse(0, true).combineEventStreams(Input.whileMouseDown(1).limit(.05)).filter(() -> uiMode == 1)
                 .filter(notOverUI).onEvent(() -> Tile.tilesNear(Input.getMouse(), brushSize.value.get()).forEach(t -> {
-            Color4 color = getColor.get();
-            switch (typeSelector.chosen.get()) {
-                case "Tile":
-                    t.color = color;
-                    break;
-                case "Block":
-                    t.drawable = new Drawable(color, "box", t);
-                    break;
-                case "Creature":
-                    t.drawable = new Creature(color, t);
-                    break;
-                case "Clear":
-                    t.drawable = null;
-                    break;
-            }
+            t.color = getColor.get();
         }));
         Input.whenMouse(2, true).filter(() -> uiMode == 1).filter(notOverUI).onEvent(() -> Tile.tileAt(Input.getMouse()).ifPresent(t -> {
             Color4 c = t.color;
-            red.value.set((int) Math.round(c.r * 32));
-            green.value.set((int) Math.round(c.g * 32));
-            blue.value.set((int) Math.round(c.b * 32));
+            height.value.set((int) Math.round(c.r * 64 - 32));
         }));
-//        Input.whileKeyDown(Keyboard.KEY_B).filter(() -> uiMode == 1)
-//                .filter(notOverUI).onEvent(() -> Tile.tilesNear(Input.getMouse(), 3).forEach(t -> {
-//            Color4 color = getColor.get();
-//            switch (typeSelector.chosen.get()) {
-//                case "Tile":
-//                    t.color = color;
-//                    sendToAll(10, t.x, t.y, color);
-//                    break;
-//                case "Block":
-//                    t.drawable = new Drawable(color, "box", t);
-//                    sendToAll(11, t.x, t.y, color);
-//                    break;
-//                case "Creature":
-//                    t.drawable = new Creature(color, t);
-//                    sendToAll(12, t.x, t.y, color);
-//                    break;
-//                case "Clear":
-//                    t.drawable = null;
-//                    sendToAll(13, t.x, t.y);
-//                    break;
-//            }
-//        }));
         return ui;
     }
 }
