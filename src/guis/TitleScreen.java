0eed14fc577c256dd9f592fa615db12402873f18
RSoiffer16 <RSoiffer16@S-LIBRARY16.jesuitpdx.org>
2016-04-15 13:35:15 -0700

    Attempts to add the main menu GUI. Better.

--- src/guis/TitleScreen.java
+++ src/guis/TitleScreen.java
@@ -30,12 +30,12 @@ public class TitleScreen extends ComponentInputGUI{
         super(n);
         dim = d;
         bDim = new Vec2(10, 10);
-        bMid = new Vec2((d.x / 2) - bDim.x, (d.y / 2) - (bDim.y));
+        bMid = d.divide(2).subtract(bDim.divide(2));
         
         components.add(new GUIPanel("back", Vec2.ZERO, dim, Color4.BLACK));
-        components.add(new GUIPanel("options Panel", bMid, bMid.add(bDim), Color4.ORANGE));
-        components.add(new GUIPanel("servers Panel", bMid.add(new Vec2(0, bDim.y)), bMid.add(bDim).add(new Vec2(0, bDim.y)), Color4.ORANGE.multiply(0.75)));
-        components.add(new GUIPanel("play Panel", bMid.add(new Vec2(0, bDim.y * 2)), bMid.add(bDim).add(new Vec2(0, bDim.y * 2)), Color4.ORANGE.multiply(0.5)));
+        components.add(new GUIPanel("options Panel", bMid, bDim, Color4.RED));
+        components.add(new GUIPanel("servers Panel", bMid.add(new Vec2(0, bDim.y)), bDim, Color4.ORANGE.multiply(0.75)));
+        components.add(new GUIPanel("play Panel", bMid.add(new Vec2(0, bDim.y * 2)), bDim, Color4.ORANGE.multiply(0.5)));
         
         options = null;
         servers = null;
